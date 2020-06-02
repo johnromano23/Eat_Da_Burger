a447bfcf4bad18bf9d14ea6b937af48434b0715a
John Romano <john_romano23@yahoo.com>
2020-06-02 00:20:06 -0400

    first commit, getting 'module not found' errors on controllers

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
